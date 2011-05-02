13235eace0f90ec911e0d2b583935fd193e48633
Ale Mako <alenight5@gmail.com>
2011-05-02 11:47:17 -0300

    Generate rspec files

--- Gemfile
+++ Gemfile
@@ -29,3 +29,6 @@ gem 'sqlite3'
 # group :development, :test do
 #   gem 'webrat'
 # end
+group :development, :test do
+  gem 'rspec-rails', '~> 2.5.x'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ LabancaAds::Application.routes.draw do
   get "pages/home"
 
   get "pages/about"
-  match "about", :to => "pages#about"home
+  match "about", :to => "pages#about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -61,5 +61,4 @@ LabancaAds::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-  match ":page", :controller => :pages, :action => page
 end

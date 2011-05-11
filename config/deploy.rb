aeba369d8bb4f27921e975fe378d45f5cd0fd1fb
Ale Mako <alenight5@gmail.com>
2011-05-11 11:24:11 -0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,19 @@
+default_run_options[:pty] = true
 set :application, "labanca-ads"
 set :repository,  "git@github.com:alenight5/labanca-ads.git"
+set :use_sudo, false
 
 set :scm, :git
+set :deploy_via, :remote_cache
+ssh_options[:forward_agent] = true
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 role :web, "localhost"                          # Your HTTP server, Apache/etc
 role :app, "localhost"                          # This may be the same as your `Web` server
 role :db,  "localhost", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
+set :deploy_to, "/home/curso4/LaBanca/labanca-ads-deploy"
+set :branch, "master"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
